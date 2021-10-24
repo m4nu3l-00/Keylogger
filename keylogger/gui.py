8136c97773820c5a278d69a243fdd7d717fc0acb
Kai <kai.weinmann@freenet.de>
2021-10-24 13:43:28 +0200

    locks

--- keylogger/gui.py
+++ keylogger/gui.py
@@ -1,5 +1,6 @@
 import sys
 import threading
+import time
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
@@ -10,19 +11,13 @@ from control import Control
 class GUI(View):
     def __init__(self):
         self.__control = None
+        self.__click_lock = threading.Lock()
 
         self.__app = QtWidgets.QApplication(sys.argv)
         self.__app.setStyle("Fusion")
         self.__window = QtWidgets.QWidget()
         screen_size = QtWidgets.QDesktopWidget().screenGeometry(-1)
         self.__window.setWindowIcon(QtGui.QIcon('icon.png'))
-        app_icon = QtGui.QIcon()
-        app_icon.addFile('icon.png', QtCore.QSize(16, 16))
-        app_icon.addFile('icon.png', QtCore.QSize(24, 24))
-        app_icon.addFile('icon.png', QtCore.QSize(32, 32))
-        app_icon.addFile('icon.png', QtCore.QSize(48, 48))
-        app_icon.addFile('icon.png', QtCore.QSize(256, 256))
-        self.__app.setWindowIcon(app_icon)
 
         self.__window.resize(screen_size.width() * 0.11, screen_size.height() * 0.03)
         size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
@@ -106,39 +101,44 @@ class GUI(View):
         self.__set_button.setEnabled(True)
 
     def __clicked_start(self):
-        if self.__start_button.text() == "Start":
-            self.__start_button.setText("Stop")
-            self.__set_button.setEnabled(False)
-            if not self.__control.start():
-                QtWidgets.QMessageBox.critical(self.__window, "Error!", "New End-Key could not be set.")
-        elif self.__start_button.text() == "Stop":
-            self.__start_button.setText("Start")
-            self.__set_button.setEnabled(True)
-            if not self.__control.stop():
-                QtWidgets.QMessageBox.critical(self.__window, "Error!", "New End-Key could not be set.")
+        with self.__click_lock:
+            if self.__start_button.text() == "Start":
+                self.__start_button.setText("Stop")
+                self.__set_button.setEnabled(False)
+                if not self.__control.start():
+                    QtWidgets.QMessageBox.critical(self.__window, "Error!", "New End-Key could not be set.")
+            elif self.__start_button.text() == "Stop":
+                self.__start_button.setText("Start")
+                self.__set_button.setEnabled(True)
+                if not self.__control.stop():
+                    QtWidgets.QMessageBox.critical(self.__window, "Error!", "New End-Key could not be set.")
 
     def __clicked_set_key(self):
+        self.__click_lock.acquire()
+
         self.__start_button.setEnabled(False)
         self.__set_button.setEnabled(False)
         self.__end_key_output.setEnabled(False)
         self.__end_key_label.setEnabled(False)
 
         self.__end_key_output.setText("Please press a key!")
-        self.__end_key_output.setFont(QtGui.QFont("Arial", 11))
+        #self.__end_key_output.setFont(QtGui.QFont("Arial", 11))
         self.__end_key_output.setStyleSheet("color: blue;")
 
-        self.__writer_thread = threading.Thread(target=self.__set_stop_key)
-        self.__writer_thread.daemon = True
-        self.__writer_thread.start()
+        stop_key_thread = threading.Thread(target=self.__set_stop_key)
+        stop_key_thread.daemon = True
+        stop_key_thread.start()
 
     def __set_stop_key(self):
         if not self.__control.set_stop_key():
             QtWidgets.QMessageBox.critical(self.__window, "Error!", "New End-Key could not be set.")
         self.__end_key_output.setText(self.__control.get_stop_key())
 
-        self.__end_key_output.setFont(QtGui.QFont("Arial", 12))
+        #self.__end_key_output.setFont(QtGui.QFont("Arial", 12))
         self.__end_key_output.setStyleSheet("color: black;")
         self.__start_button.setEnabled(True)
         self.__set_button.setEnabled(True)
         self.__end_key_output.setEnabled(True)
         self.__end_key_label.setEnabled(True)
+
+        self.__click_lock.release()
