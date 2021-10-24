a1e53ac17be8391f070dffd7036b944fd382d6a4
Kai <kai.weinmann@freenet.de>
2021-10-24 16:57:52 +0200

    big gui fix

--- keylogger/gui.py
+++ keylogger/gui.py
@@ -1,8 +1,6 @@
-import sys
 import threading
-import time
-
-from PyQt5 import QtCore, QtGui, QtWidgets
+import tkinter as tk
+from tkinter import messagebox
 
 from view import View
 from control import Control
@@ -11,119 +9,103 @@ from control import Control
 class GUI(View):
     def __init__(self):
         self.__control = None
-        self.__click_lock = threading.Lock()
+        self.__state = "Start"
 
-        self.__app = QtWidgets.QApplication(sys.argv)
-        self.__app.setStyle("Fusion")
-        self.__window = QtWidgets.QWidget()
-        screen_size = QtWidgets.QDesktopWidget().screenGeometry(-1)
-        self.__window.setWindowIcon(QtGui.QIcon('icon.png'))
+        self.__window = tk.Tk()
+        #self.__window.geometry('300x' + str(int(self.__window.winfo_screenwidth()*0.2)))
+        self.__window.resizable(True, False)
+        self.__window.title('Keylogger')
+        self.__window.protocol("WM_DELETE_WINDOW", self.__end_after_close)
 
-        self.__window.resize(screen_size.width() * 0.11, screen_size.height() * 0.03)
-        size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-        size_policy.setHorizontalStretch(1)
-        size_policy.setVerticalStretch(1)
-        size_policy.setHeightForWidth(self.__window.sizePolicy().hasHeightForWidth())
-        self.__window.setSizePolicy(size_policy)
-        self.__window.setContextMenuPolicy(QtCore.Qt.PreventContextMenu)
-        self.__window.setWindowTitle("Keylogger")
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.__window)
-        self.verticalLayout.setObjectName("verticalLayout")
-        translate = QtCore.QCoreApplication.translate
+        self.__start_button = tk.Button(
+            self.__window,
+            text='Start',
+            command=self.__clicked_start
+        )
+        self.__start_button.pack(
+            padx=10,
+            pady=2,
+            fill='both'
+        )
 
-        # StartButton
-        self.__start_button = QtWidgets.QPushButton(self.__window)
-        size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-        size_policy.setHorizontalStretch(1)
-        size_policy.setVerticalStretch(1)
-        size_policy.setHeightForWidth(self.__start_button.sizePolicy().hasHeightForWidth())
-        self.__start_button.setSizePolicy(size_policy)
-        self.__start_button.setObjectName("SetButton")
-        self.verticalLayout.addWidget(self.__start_button)
-        self.__start_button.setText(translate("Keylogger", "Start"))
+        self.__set_button = tk.Button(
+            self.__window,
+            text='Set end-key',
+            command=self.__clicked_set_key
+        )
+        self.__set_button.pack(
+            padx=10,
+            pady=2,
+            fill='both'
+        )
 
-        # SetButton
-        self.__set_button = QtWidgets.QPushButton(self.__window)
-        size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-        size_policy.setHorizontalStretch(1)
-        size_policy.setVerticalStretch(1)
-        size_policy.setHeightForWidth(self.__set_button.sizePolicy().hasHeightForWidth())
-        self.__set_button.setSizePolicy(size_policy)
-        self.__set_button.setObjectName("SetButton")
-        self.verticalLayout.addWidget(self.__set_button)
-        self.__set_button.setText(translate("Keylogger", "Set end-key"))
+        self.__end_key_label = tk.Label(
+            self.__window,
+            text='End-key:',
+            anchor='sw',
+            justify=tk.LEFT
+        )
+        self.__end_key_label.pack(
+            padx=10,
+            pady=(5, 0),
+            fill='both'
+        )
 
-        # EndKeyLabel
-        self.__end_key_label = QtWidgets.QLabel(self.__window)
-        size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-        size_policy.setHorizontalStretch(1)
-        size_policy.setVerticalStretch(1)
-        size_policy.setHeightForWidth(self.__end_key_label.sizePolicy().hasHeightForWidth())
-        self.__end_key_label.setSizePolicy(size_policy)
-        self.__end_key_label.setObjectName("label")
-        self.verticalLayout.addWidget(self.__end_key_label)
-        self.__end_key_label.setText("End-key:")
-
-        # EndKeyOutput
-        self.__end_key_output = QtWidgets.QLineEdit(self.__window)
-        size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-        size_policy.setHorizontalStretch(1)
-        size_policy.setVerticalStretch(1)
-        size_policy.setHeightForWidth(self.__end_key_output.sizePolicy().hasHeightForWidth())
-        self.__end_key_output.setSizePolicy(size_policy)
-        self.__end_key_output.setReadOnly(True)
-        self.__end_key_output.setObjectName("EndKeyOutput")
-        self.verticalLayout.addWidget(self.__end_key_output)
-        self.__end_key_output.setText(translate("Keylogger", "EndKeyOutput"))
-        self.__end_key_output.setAlignment(QtCore.Qt.AlignCenter)
-
-        # Events
-        self.__start_button.clicked.connect(self.__clicked_start)
-        self.__set_button.clicked.connect(self.__clicked_set_key)
+        self.__end_key_text = tk.Text(
+            self.__window,
+            height=1
+        )
+        self.__end_key_text.insert('1.0', "Loading...")
+        self.__end_key_text.pack(
+            padx=10,
+            pady=5,
+            fill='both'
+        )
 
     def start_view(self, control: Control):
         self.__control = control
-        self.__end_key_output.setText(self.__control.get_stop_key())
-        self.__app.aboutToQuit.connect(self.end_after_close)
-        self.__window.show()
-        sys.exit(self.__app.exec())
+        self.__end_key_text.delete('1.0', tk.END)
+        self.__end_key_text.insert('1.0', control.get_stop_key())
+        self.__end_key_text['state'] = 'disabled'
+        while True:
+            if self.__state == "Start":
+                self.__start_button.config(text="Start")
+                self.__set_button["state"] = "normal"
+            elif self.__state == "Stop":
+                self.__start_button.config(text="Stop")
+                self.__set_button["state"] = "disabled"
+            self.__window.update()
 
-    def end_after_close(self):
+    def __end_after_close(self):
         if self.__control.keylogger_is_running():
             if self.__control.stop():
-                QtWidgets.QMessageBox.about(self.__window, "Keylogger Stopped", "Keylogger has been stopped")
+                messagebox.showinfo("Keylogger Stopped", "Keylogger has been stopped.")
             else:
-                QtWidgets.QMessageBox.critical(self.__window, "Error!", "Can't stop Keylogger before Closing")
-        return sys.argv
+                messagebox.showerror("Error!", "Can't stop Keylogger before Closing.")
+        self.__window.destroy()
 
     def show_keylogger_stopped(self):
-        self.__start_button.setText("Start")
-        self.__set_button.setEnabled(True)
+        self.__state = "Start"
 
     def __clicked_start(self):
-        with self.__click_lock:
-            if self.__start_button.text() == "Start":
-                self.__start_button.setText("Stop")
-                self.__set_button.setEnabled(False)
-                if not self.__control.start():
-                    QtWidgets.QMessageBox.critical(self.__window, "Error!", "New End-Key could not be set.")
-            elif self.__start_button.text() == "Stop":
-                self.__start_button.setText("Start")
-                self.__set_button.setEnabled(True)
-                if not self.__control.stop():
-                    QtWidgets.QMessageBox.critical(self.__window, "Error!", "New End-Key could not be set.")
+        if self.__start_button['text'] == "Start":
+            self.__state = "Stop"
+            if not self.__control.start():
+                messagebox.showerror("Error!", "Keylogger could not be started.")
+        elif self.__start_button['text'] == "Stop":
+            self.__state = "Start"
+            if not self.__control.stop():
+                messagebox.showerror("Error!", "Keylogger could not be stopped.")
 
     def __clicked_set_key(self):
-        self.__click_lock.acquire()
+        self.__start_button["state"] = "disabled"
+        self.__set_button["state"] = "disabled"
 
-        self.__start_button.setEnabled(False)
-        self.__set_button.setEnabled(False)
-        self.__end_key_output.setEnabled(False)
-        self.__end_key_label.setEnabled(False)
-
-        self.__end_key_output.setText("Please press a key!")
-        #self.__end_key_output.setFont(QtGui.QFont("Arial", 11))
-        self.__end_key_output.setStyleSheet("color: blue;")
+        self.__end_key_text["state"] = "normal"
+        self.__end_key_text.delete('1.0', tk.END)
+        self.__end_key_text.insert('1.0', "Please press a key!")
+        self.__end_key_text.configure(fg="blue")
+        self.__end_key_text['state'] = 'disabled'
 
         stop_key_thread = threading.Thread(target=self.__set_stop_key)
         stop_key_thread.daemon = True
@@ -131,14 +113,12 @@ class GUI(View):
 
     def __set_stop_key(self):
         if not self.__control.set_stop_key():
-            QtWidgets.QMessageBox.critical(self.__window, "Error!", "New End-Key could not be set.")
-        self.__end_key_output.setText(self.__control.get_stop_key())
+            messagebox.showerror("Error!", "New End-Key could not be set.")
+        self.__end_key_text["state"] = "normal"
+        self.__end_key_text.delete('1.0', tk.END)
+        self.__end_key_text.insert('1.0', self.__control.get_stop_key())
+        self.__end_key_text.configure(fg="black")
+        self.__end_key_text['state'] = 'disabled'
 
-        #self.__end_key_output.setFont(QtGui.QFont("Arial", 12))
-        self.__end_key_output.setStyleSheet("color: black;")
-        self.__start_button.setEnabled(True)
-        self.__set_button.setEnabled(True)
-        self.__end_key_output.setEnabled(True)
-        self.__end_key_label.setEnabled(True)
-
-        self.__click_lock.release()
+        self.__start_button["state"] = "normal"
+        self.__set_button["state"] = "normal"
