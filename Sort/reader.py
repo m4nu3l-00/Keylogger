db27293fe848ceaf392b0326cc72624ddee49322
Kai <kai.weinmann@freenet.de>
2021-11-01 19:24:42 +0100

    data type fix while reading csv

--- Sort/reader.py
+++ Sort/reader.py
@@ -9,7 +9,7 @@ class CsvReader:
             event_array = []
             reader = csv.reader(csv_file, delimiter=',')
             for csv_data in reader:
-                data[1] = float(data[1])
+                csv_data[1] = float(csv_data[1])
                 csv_data[2] = csv_data[2] == 'True'
                 event_array.append(csv_data)
             return event_array
